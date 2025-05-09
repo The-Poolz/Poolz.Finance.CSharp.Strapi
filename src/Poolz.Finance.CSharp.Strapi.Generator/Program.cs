00076ef5544cfd02a8aea11ca9d2c05dcafc624b
ArdenHide <ardenhide@gmail.com>
2025-05-09 12:34:27 +0300

    - create C# project for code generation

---
+++ src/Poolz.Finance.CSharp.Strapi.Generator/Program.cs
@@ -0,0 +1,14 @@
+﻿using GraphQlClientGenerator;
+using EnvironmentManager.Extensions;
+using Poolz.Finance.CSharp.Strapi.Generator;
+
+var url = Env.GRAPHQL_STRAPI_URL.GetRequired();
+var schema = await GraphQlGenerator.RetrieveSchema(HttpMethod.Post, url);
+var configuration = new GraphQlGeneratorConfiguration
+{
+    TargetNamespace = Env.TARGET_NAMESPACE.GetRequired(),
+    IdTypeMapping = IdTypeMapping.String
+};
+var generator = new GraphQlGenerator(configuration);
+var csharpCode = generator.GenerateFullClientCSharpFile(schema);
+await File.WriteAllTextAsync(Env.GENERATED_FILE_PATH.GetRequired(), csharpCode);
