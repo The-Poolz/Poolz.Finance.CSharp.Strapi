19e720cf2b1bfa5fba9c9671d6de53f146c79051
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-05-19 17:34:17 +0300

    Handle graphql `DateTime` type as csharp `DateTime` (#39)
    * Handle graphql `DateTime` type as csharp `DateTime`
    
    * - rename to `ScalarFieldTypeMappingProvider`
    
    * - typo

---
+++ src/Poolz.Finance.CSharp.Strapi.Generator/ScalarFieldTypeMappingProvider.cs
@@ -0,0 +1,21 @@
+﻿using GraphQlClientGenerator;
+
+namespace Poolz.Finance.CSharp.Strapi.Generator;
+
+public sealed class ScalarFieldTypeMappingProvider : IScalarFieldTypeMappingProvider
+{
+    public ScalarFieldTypeDescription GetCustomScalarFieldType(ScalarFieldTypeProviderContext context)
+    {
+        var unwrapped = context.FieldType.UnwrapIfNonNull();
+
+        return unwrapped.Name switch
+        {
+            "DateTime" => new ScalarFieldTypeDescription
+            {
+                NetTypeName = GenerationContext.GetNullableNetTypeName(context, "DateTime", isReferenceType: false),
+                FormatMask = null
+            },
+            _ => DefaultScalarFieldTypeMappingProvider.GetFallbackFieldType(context)
+        };
+    }
+}
